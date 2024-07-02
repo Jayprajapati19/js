1a3ba1d3b8c4c307d002bc7cba95222c3d9b7e9e
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-02 17:07:17 +0530

    summery

--- revision2/01_basics/06_memory.js
+++ revision2/01_basics/06_memory.js
@@ -1,5 +1,6 @@
-// Stack (Primitive) --> ye memory use hoti tab jo hamne variable declarerd kiya hai uski copy milti hai,
-// Heap(Non - Primitive) --> Refrence of original value , jo bhi chgane  karenge wo original value me change hoga
+// Stack (Primitive) --> ye memory use hoti tab jo hamne variable declarerd kiya hai uski copy milti hai, ==> copy male
+
+// Heap(Non - Primitive) --> Refrence of original value , jo bhi chgane  karenge wo original value me change hoga => refrence male update thay e original value ma j thay
 
 let myYTName = "jayprajapati.yt"
 
