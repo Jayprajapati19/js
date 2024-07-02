d9f363db779398d27a28a412a7343f2ba2fdf5aa
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-02 14:37:22 +0530

    conversion

---
+++ revision2/01_basics/03_conversion_opearations.js
@@ -0,0 +1,41 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "jay"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNum = Number(score)
+
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+
+// "33"=>33
+// "33abc" => NaN
+// true => 1, false=> 0
+
+// let isLoggedIn = 1 //true
+// let isLoggedIn = "" //false
+let isLoggedIn = "jay" //false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "jay" => true
+
+
+let someNumber = 33
+
+let stringNum = String(someNumber)
+
+// console.log(typeof stringNum);
+
+// console.log(stringNum);
+
