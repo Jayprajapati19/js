7794ce3f6d33539f8a0956ee4c284dfa3dad5ef6
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-02 15:28:46 +0530

    practice

--- revision2/01_basics/04_comparation.js
+++ revision2/01_basics/04_comparation.js
@@ -16,3 +16,11 @@
 // console.log(undefined >= 0);
 // console.log(undefined <= 0);
 // console.log(undefined != 0);
+
+/*
+
+Double equals(==) will perform a type conversion when comparing two things, and will handle NaN, -0, and + 0 specially to conform to IEEE 754(so NaN != NaN, and - 0 == +0);
+
+Triple equals(===) will do the same comparison as double equals(including the special handling for NaN, -0, and + 0) but without type conversion; if the types differ, false is returned.
+
+*/
