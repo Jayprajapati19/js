faef824233d0347c4e0b002fe4a8bf1a287b9474
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-03 21:36:10 +0530

    scope

--- revision2/02_basic/06_scopes.js
+++ revision2/02_basic/06_scopes.js
@@ -7,11 +7,53 @@ if (true) {
     const b = 20
     // console.log("INNER:", a);
 }
-
-
 // console.log(a);
 // console.log(b);
 // console.log(a);
+
+// Nested Scope
+
+function one() {
+    const userName = "jay"
+
+    function two() {
+        const website = "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+
+    // two()
+}
+
+// one()
+
+if (true) {
+    const username = "jay"
+    if (username === "jay") {
+        const website = " youtube"
+        // console.log(username + website);
+
+    }
+    // console.log(website);
+}
+
+// console.log(userName);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Hoisting introoo
+
+// console.log(addOne(5))
+// function addOne(num) {
+//     return num + 1;
+// }
+
+
+// addTwo(5)
+// const addTwo = function (num) {
+//     return num + 2;
+// }
 
 
 
