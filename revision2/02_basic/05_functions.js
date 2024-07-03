c088fb62a5fdaa1043a6cdf5bfdff9a874e5cb56
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-03 17:10:12 +0530

    functions

--- revision2/02_basic/05_functions.js
+++ revision2/02_basic/05_functions.js
@@ -41,9 +41,38 @@ function loginUserMessage(username = "sam") {
 }
 
 // console.log(loginUserMessage("jayy"))
-console.log(loginUserMessage("gudu"))
+// console.log(loginUserMessage("gudu"))
+
+function calculateCardPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCardPrice(200, 300, 900, 10s09, 440, 1909))
+
+// ...rest,spred operator
+
+const user = {
+    username: "jay",
+    prices: 193
+}
+
+function handleObj(anyObj) {
+    // console.log(`Username is ${anyObj.username} and Price is ${anyObj.price}`);
+}
 
 
+// // handleObj(user)
+// handleObj({
+//     username: "sam",
+//     price: 200
+// })
 
+const myArr = [200, 400, 100, 190]
 
+function returnSecondValue(getArr) {
+    return getArr[1]
+}
+
+// console.log(returnSecondValue(myArr));
+// console.log(returnSecondValue([200, 400]));
 
