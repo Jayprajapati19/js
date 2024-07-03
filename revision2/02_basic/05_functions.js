ee63da6571367410a792d7de1d914af159034a46
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-03 15:56:25 +0530

    functions and project

---
+++ revision2/02_basic/05_functions.js
@@ -0,0 +1,36 @@
+
+
+// console.log("j");
+
+function sayMyName() {
+    console.log("j");
+    console.log("a");
+    console.log("y");
+    console.log("p");
+    console.log("r");
+    console.log("a");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(num1, num2) {
+//      console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1, num2) {
+
+    // let result = num1 + num2;
+    // console.log("jay");
+    // return result;
+
+    return num1 + num2;
+
+}
+
+const result = addTwoNumbers(3, 9)
+
+console.log("Result: ", result);
+
+
+
+
