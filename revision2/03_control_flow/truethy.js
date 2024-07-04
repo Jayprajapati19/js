20c4b4d623ee6b37eea4a8c90d870fcf707104b3
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-04 22:12:51 +0530

    teaniary

--- revision2/03_control_flow/truethy.js
+++ revision2/03_control_flow/truethy.js
@@ -36,12 +36,22 @@ let val1;
 // val1 = undefined ?? 19
 
 // val1 = null ?? 10 ?? 50
+// console.log(val1);
+
+// Terniary Operator  +++++++++++++
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
 
 
 
-console.log(val1);
+
+
+
 
 
 
