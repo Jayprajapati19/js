722c5d4edb717181235a3a5664f85a084b11d41e
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-04 15:42:56 +0530

    control folw

---
+++ revision2/03_control_flow/one.js
@@ -0,0 +1,32 @@
+//  if
+
+const isUserLoggedIn = true
+const temp = 41
+
+if (temp === 40) {
+    // console.log("less than 50");
+} else {
+
+    // console.log("temp is greaten than 50");
+}
+// console.log("Excuted");
+
+// <, >, <=, >=, ==, !=, ===, !==, 
+
+const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+
+
+
+
+
+
+
