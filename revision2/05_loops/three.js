4288b253d38d8d637c054cfcd4f867a7502e6a87
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-08 21:19:59 +0530

    maps

--- revision2/05_loops/three.js
+++ revision2/05_loops/three.js
@@ -16,6 +16,34 @@ const greetings = "Hello world!"
 //     console.log(`Each char is ${greet}`);
 // }
 
-// Maps
+// ++++++++++++++++++++++++++++++++++++++++++
 
+// Maps
+// The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+
+//  it is unique in the Map's collection
+
+const map = new Map()
+
+map.set('IN', 'India')
+map.set('USA', 'Unites State Of America')
+map.set('JP', 'Japan')
+map.set('FR', 'France')
+map.set('IN', 'India')
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':->', value);
+}
+
+const myObj = {
+    game1: "NFS",
+    game2: "ycity",
+    game3: "GBMI"
+}
+
+// for (const [key, value] of myObj) {
+//     onsole.log(key, ':->', value);
+// }
 
