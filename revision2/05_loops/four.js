49cfb1005a56dde46624051d0595e787226d3b94
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-08 21:57:04 +0530

    for in

---
+++ revision2/05_loops/four.js
@@ -0,0 +1,20 @@
+const myObj = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    py: "Python"
+}
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+const programming = ['js', 'cpp', 'rb', 'py', 'java']
+
+for (const key in programming) {
+    console.log(key);
+}
+
+
+
+
