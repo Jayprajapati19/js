48791e7b5687871467be9563f6db77f6a21cfff5
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-08 21:10:53 +0530

    for of loop

---
+++ revision2/05_loops/three.js
@@ -0,0 +1,21 @@
+// for of  --- array specific loop
+// object in for of loop is not js object...mean of this object is kiske upr aap lopp lagana chahte honn
+
+// { "", "", "" }
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings = "Hello world!"
+
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+
+
