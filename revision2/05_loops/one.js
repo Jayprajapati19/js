a124eba34e04dccae25de752751b7bfc217269d7
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-08 17:28:34 +0530

    break

--- revision2/05_loops/one.js
+++ revision2/05_loops/one.js
@@ -9,15 +9,48 @@ for (let i = 0; i <= 10; i++) {
 
 }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`Outer value : ${i}`);
-    for (let j = 1; j <= 10; j++) {
-        // console.log(`Inner Loop value: ${j} amd onner loop ${i}`);
-        console.log(i + ' * ' + j + ' = ' + i * j); //table
-    }
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer value : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner Loop value: ${j} amd onner loop ${i}`);
+//         console.log(i + ' * ' + j + ' = ' + i * j); //table
+//     }
+// }
+
+let myArr = ["jay", "oza", "superman"]
+
+// console.log(myArr.length);
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+
+    // console.log(element);
+
 }
 
 
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         // break;
+//     }
+//     console.log(` Value of index ${index}`);
+
+
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(` Value of index ${index}`);
+
+
+}
 
 
 
