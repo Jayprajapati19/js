f63ac45ef24a5943d7bc72a5db90d53d2191a2bb
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-11 08:26:44 +0530

    practice

--- revision2/05_loops/six.js
+++ revision2/05_loops/six.js
@@ -85,9 +85,10 @@ const books = [
 ];
 
 
-const userBooks = books.filter((bk) => bk.year === 1960)
+// const userBooks = books.filter((bk) => bk.year === 1960)
+// const userBooks = books.filter((bk) => bk.publish >= "Signet Classics")
+const userBooks = books.filter((bk) => bk.year >= 1949 && bk.genre === "Coming-of-Age Fiction")
 
 console.log(userBooks);
 console.log(userBooks.length);
 
-// 8:40
