6d182fbf3d314833e38009488a98c9cec9f9eb8d
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-16 11:37:17 +0530

    this

---
+++ revision2/10_classes_and_OOP/oop.js
@@ -0,0 +1,25 @@
+// object litaral
+
+const user = {
+   username: "jay",
+   email: "jd@gmail.com",
+   age: 18,
+   signIn: true,
+
+   getUserdDetails: function () {
+      console.log("Got user details from DB");
+
+   }
+}
+console.log(user.username);
+console.log(user.getUserdDetails());
+
+
+// Current Context --> this key word
+
+
+
+
+
+
+
