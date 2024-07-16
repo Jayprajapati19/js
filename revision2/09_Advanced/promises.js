d8fe13920cf9b49d7cfa52351776ebb313d75b51
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-16 10:21:19 +0530

    fetch

--- revision2/09_Advanced/promises.js
+++ revision2/09_Advanced/promises.js
@@ -84,3 +84,24 @@ async function consumedPromiseFive() {
 
 consumedPromiseFive()
 
+// async function getAllUsers() {
+//    try {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//       const data = await response.json()
+//       console.log(data);
+//    } catch (error) {
+//       console.log("E: ", error);
+//    }
+// }
+
+// getAllUsers()
+
+fetch('https://api.github.com/users/Jayprajapati19')
+   .then((response) => {
+      return response.json()
+   })
+   .then((data) => {
+      console.log(data);
+   })
+   .catch((error) => console.log(error));
+
