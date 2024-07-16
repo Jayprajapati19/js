7e4c732af5b710048858912e62ed936058c3c67a
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-16 14:15:06 +0530

    constructor

--- revision2/10_classes_and_OOP/oop.js
+++ revision2/10_classes_and_OOP/oop.js
@@ -7,15 +7,33 @@ const user = {
    signIn: true,
 
    getUserdDetails: function () {
-      console.log("Got user details from DB");
+      // console.log("Got user details from DB");
+      // console.log(`Username: ${this.username}`);
+      console.log(this);
 
    }
 }
-console.log(user.username);
-console.log(user.getUserdDetails());
 
+// console.log(user.username);
+// console.log(user.getUserdDetails());
+// console.log(this);
 
 // Current Context --> this key word
+// new key word is constructor function -> ak hi objects litarl se multiple instances bana shako 
+
+function User(username, loginCount, isLoggedIn) {
+   this.username = username
+   this.loginCount = loginCount
+   this.isLoggedIn = isLoggedIn
+
+   return this
+}
+
+const userOne = new User("jay", 12, true)
+const userTwo = new User("diyu", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
 
 
 
