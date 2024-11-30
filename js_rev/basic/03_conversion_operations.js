afc70f88d6b1e36ee8fd6265d55d1f055464b392
Jayprajapati19 <jayp37679@gmail.com>
2024-11-30 11:54:51 +0530

    conversions

---
+++ js_rev/basic/03_conversion_operations.js
@@ -0,0 +1,40 @@
+// console.log("Jay Meladi");
+
+// let score = "33abc"; //return a NaN (Not a Number) for
+// let score = "33";
+
+// let score = null; //return a 
+// let score = undefined; //return NaN
+
+/*
+let score = true; 
+//true->return 1/ false return 0;
+
+// "19" -> 19
+// "19abs" -> NaN
+// true -> 1
+
+console.log(typeof score);
+
+let valueInNum = Number(score);
+console.log(typeof valueInNum);
+
+console.log(valueInNum);
+
+*/
+
+
+// let LoggedIn = 1; //true
+let LoggedIn = "";
+
+let boolIsLoggedIn = Boolean(LoggedIn);
+console.log(boolIsLoggedIn);
+
+
+// 1->true;  0->false
+// "" -> false
+// "jay" -> true 
+
+
+
+
