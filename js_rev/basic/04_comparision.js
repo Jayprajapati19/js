7a44df32e273a742d7ae5264d906748f3b39645f
Jayprajapati19 <jayp37679@gmail.com>
2024-11-30 15:31:50 +0530

    data types in js

---
+++ js_rev/basic/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 < -1);
+// console.log(2 <= 1);
+// console.log("seen ", 2 ^ 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2" < 1);
+// console.log("02" < 10);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null === 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+// console.log(undefined === 0);
+// console.log(undefined == 0);
+
+// console.log("2" === 2);
+// console.log("2" === "2"); //compare Data types...
---
+++ js_rev/basic/05_data_types2.js
@@ -0,0 +1,29 @@
+// Primitive data types
+
+// Changes in copy not original value
+
+// 7 - types: String, Number, Boolean, null, undefined. Symbol, BigInt
+
+const score = 100;
+
+// javaScript is a dynamically typed language.
+
+// What does "dynamically typed" mean?
+// The type of a variable is determined at runtime, not at compile time.
+// You can assign values of different types to the same variable without declaring its type explicitly.
+
+let x = 10; // x is a number
+x = "Hello"; // Now x is a string
+x = true; // Now x is a boolean
+
+console.log(x);
+console.log(typeof x);
+
+// This flexibility is a feature of dynamically typed languages but can also lead to runtime errors if not handled carefully.
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Non-Primitive types ( refrence types)
+
+// Array, Object, Functions
+// memory ne direact usaka refrence diya jata haii
