2fec76425f3e4d34e3b9b9b807c167c9fd6f2ec3
Jayprajapati19 <jayp37679@gmail.com>
2024-11-30 15:55:26 +0530

    data types

--- js_rev/basic/05_data_types2.js
+++ js_rev/basic/05_data_types2.js
@@ -3,9 +3,6 @@
 // Changes in copy not original value
 
 // 7 - types: String, Number, Boolean, null, undefined. Symbol, BigInt
-
-const score = 100;
-
 // javaScript is a dynamically typed language.
 
 // What does "dynamically typed" mean?
@@ -16,14 +13,44 @@ let x = 10; // x is a number
 x = "Hello"; // Now x is a string
 x = true; // Now x is a boolean
 
-console.log(x);
-console.log(typeof x);
+// console.log(x);
+// console.log(typeof x);
 
 // This flexibility is a feature of dynamically typed languages but can also lead to runtime errors if not handled carefully.
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outSideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(typeof outSideTemp);
+
+// console.log(id === anotherId);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // Non-Primitive types ( refrence types)
+
+const heros = ["shaktiman", "jayy", "tikooo", "bhim", "patlu"];
+
+let myObj = {
+  name: "jay",
+  age: 22,
+  city: "ahm",
+};
+
+const myFunc = function () {
+  console.log("Hello World!");
+};
+
+// console.log(typeof myObj);
+// console.log(typeof myFunc);
+// console.log(typeof heros);
 
 // Array, Object, Functions
 // memory ne direact usaka refrence diya jata haii
