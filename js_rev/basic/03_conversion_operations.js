532cd817590386b25e0a9aae70b0f6a76c29fec4
Jayprajapati19 <jayp37679@gmail.com>
2024-11-30 14:57:27 +0530

    clear

--- js_rev/basic/03_conversion_operations.js
+++ js_rev/basic/03_conversion_operations.js
@@ -3,7 +3,7 @@
 // let score = "33abc"; //return a NaN (Not a Number) for
 // let score = "33";
 
-// let score = null; //return a 
+// let score = null; //return a
 // let score = undefined; //return NaN
 
 /*
@@ -23,30 +23,25 @@ console.log(valueInNum);
 
 */
 
-
 // let LoggedIn = 1; //true
 let LoggedIn = "";
 
 let boolIsLoggedIn = Boolean(LoggedIn);
 // console.log(boolIsLoggedIn);
 
-
 // 1->true;  0->false
 // "" -> false
-// "jay" -> true 
-
+// "jay" -> true
 
 // let SomeNum =19;
 // console.log(typeof SomeNum);
 
-
 // let StrNum=String(SomeNum);
 // console.log(typeof StrNum);
 
-
 // +++++++++++++++++++++++++++++ Operations +++++++++++++++++++++++++++++++
- 
-let value =3;
+
+let value = 3;
 let negValue = -value;
 // console.log(negValue);
 
@@ -59,6 +54,37 @@ console.log(2/2);
 console.log(2%2);//reminder
 */
 
+let str1 = "Hello";
+let str2 = " Jay";
 
-let str1 = "hello"; 
+let str3 = str1 + str2;
+// console.log(str3);
 
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); //first je opration hase e perform thasee
+
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(+true);
+// console.log(true+);
+
+// console.log(+""); //return 0
+
+// ++++++++++++++++++++++++++
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// +++++++++++++++++++++++
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
