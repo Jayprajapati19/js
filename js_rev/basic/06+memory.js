43f2880eece6033a403bff2f6f52d8594cd750db
Jayprajapati19 <jayp37679@gmail.com>
2024-11-30 21:32:46 +0530

    stirng in js

---
+++ js_rev/basic/06+memory.js
@@ -0,0 +1,15 @@
+// two typs -> Stack(primitive) --> yaha se copy milta haii and Heap(non-primitive) --> yaha se refrence milta hai
+
+let myYtname = "JayPrajapatidot.com";
+
+let anotherYtname = myYtname;
+
+anotherYtname = "Chai Our Jay.com";
+
+console.log(myYtname);
+console.log(anotherYtname);
+
+let user = {
+  email: "Jayp1909@gmail.com",
+  usiId: 1909,
+};
