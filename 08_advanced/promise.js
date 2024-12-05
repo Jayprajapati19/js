c490f44e00484775163886bbdb04fde7738a94f6
Jayprajapati19 <jayp37679@gmail.com>
2024-12-05 21:21:39 +0530

    fetch

--- 08_advanced/promise.js
+++ 08_advanced/promise.js
@@ -22,3 +22,82 @@ new Promise((resolve, reject) => {
 }).then(() => {
   console.log("Async Two Resolved..");
 });
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ username: "jay", email: "DevJp@gmail.com" });
+  }, 1000);
+});
+
+promiseThree.then((user) => {
+  console.log(user);
+});
+
+const promiseFour = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "DevJP", password: "123" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 2000);
+});
+
+// call-back hell, chaining
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => console.log("The promise either resolve or rejected "));
+
+const promiseFive = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "JS", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumedPromisedFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumedPromisedFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E: ", error);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://api.github.com/users/Jayprajapati19")
+  .then((res) => {
+    return res.json();
+  })
+
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
